6d9559af059fc2387ec0f678ec06dbdfdaa62939
Gabriel Peal <gpeal@google.com>
2012-06-27 10:14:21 -0400

    Bug Fix: Adds a bunch of dependencies to WebInspector required to load saved timeline data

--- TimelineEmbedded.js
+++ TimelineEmbedded.js
@@ -1,6 +1,11 @@
 WebInspector.TimelineEmbedded = function() {
 	WebInspector.timelineManager = new WebInspector.TimelineManager();
+	//needed for the timeline panel
 	WebInspector.shortcutsScreen = new WebInspector.ShortcutsScreen();
+	WebInspector.networkManager = new WebInspector.NetworkManager();
+	WebInspector.console = new WebInspector.ConsoleModel();
+	WebInspector.resourceTreeModel = new WebInspector.ResourceTreeModel(WebInspector.networkManager);
+	WebInspector.debuggerModel = new WebInspector.DebuggerModel();
 	this.timelinePanel = new WebInspector.TimelinePanel();
 }
 
@@ -19,5 +24,8 @@ function embedTimeline() {
 	timelineDiv.__view = null;
 	timelineDiv.addStyleClass("visible");
 	mainPanelsDiv.appendChild(timelineDiv);
+	//because this isn't running inside of a fully fledged inspector, some of the methods that set all of the panels styles aren't called
+	this.timelineEmbedded.timelinePanel.splitView._restoreSidebarWidth()
+
 	console.log("created timeline");
 }
