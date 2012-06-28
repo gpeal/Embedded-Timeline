c1a7e6bfe7d5fce91e2a737faeb29ebe7deafa66
Gabriel Peal <gpeal@google.com>
2012-06-28 17:37:41 -0400

    Bug Fix: timeline links should open in a new tab Bug Fix: removed a console debug log Bug Fix: removed an unneccesary js file

--- ResourceUtils.js
+++ ResourceUtils.js
@@ -311,7 +311,7 @@ WebInspector.formatLinkText = function(url, lineNumber)
 WebInspector.linkifyResourceAsNode = function(url, lineNumber, classes, tooltipText)
 {
     var linkText = WebInspector.formatLinkText(url, lineNumber);
-    var anchor = WebInspector.linkifyURLAsNode(url, linkText, classes, false, tooltipText);
+    var anchor = WebInspector.linkifyURLAsNode(url, linkText, classes, true, tooltipText);
     anchor.preferredPanel = "resources";
     anchor.lineNumber = lineNumber;
     return anchor;
--- TimelineEmbedded.js
+++ TimelineEmbedded.js
@@ -48,7 +48,6 @@ var requestTimelineLog = function() {
     xhr.open("GET", "Logs/timeline_data", false);
     xhr.send(null);
     if(xhr.status == 200) {
-    	console.log("Loading Timeline Data");
     	var data = JSON.parse(xhr.responseText);
         WebInspector.timelinePanel._model.reset();
         WebInspector.timelinePanel._model._loadNextChunk(data, 1);
