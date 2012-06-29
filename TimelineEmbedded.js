378f4bdc0bfb06c48a01e1c62cef22e7aa01de40
Gabriel Peal <gpeal@google.com>
2012-06-29 10:20:23 -0400

    Bug Fix: the entire timeline loads correctly at first load now (doesn't require switching to memory and back)

--- TimelineEmbedded.js
+++ TimelineEmbedded.js
@@ -12,6 +12,7 @@ WebInspector.loadTimelineResources = function() {
 	this.timelinePanel._overviewPane._frameOverview = new WebInspector.TimelineFrameOverview(this.timelinePanel._model);
 
 	this._frameController = new WebInspector.TimelineFrameController(this.timelinePanel._model, this.timelinePanel._overviewPane, this.timelinePanel._presentationModel);
+	this.timelinePanel._overviewPane._heapGraph._isShowing = false;
 }
 
 var loaded = function() {
@@ -40,7 +41,6 @@ var embedTimeline = function() {
 	//because this isn't running inside of a fully fledged inspector, some of the methods that set all of the panels styles aren't called
 	WebInspector.timelinePanel.splitView._restoreSidebarWidth()
 	WebInspector.timelinePanel.wasShown();
-	WebInspector.timelinePanel._overviewPane._update();
 }
 
 var requestTimelineLog = function() {
@@ -52,4 +52,14 @@ var requestTimelineLog = function() {
         WebInspector.timelinePanel._model.reset();
         WebInspector.timelinePanel._model._loadNextChunk(data, 1);
     }
+    //I guess the frames take a while to load in so if you try to redraw the canvas too soon
+    //it will only draw some of the frames
+	setTimeout(paintCategoryStrips, 1000);
+}
+
+var paintCategoryStrips = function() {
+	//An annoying hack required because the timeline needs to re-switch to the Events view but
+	//it can only switch to the events view if it isn't currently in it
+	WebInspector.timelinePanel._overviewPane.setMode("Memory");
+	WebInspector.timelinePanel._overviewPane.setMode("Events");
 }
